0d6ab4eaf44121c8c3c46a733cd409b2774c8cb8
Facedown <klapuchdominik@gmail.com>
2016-05-28 19:04:32 +0200

    Made Fake more flexible

--- Core/Fake.php
+++ Core/Fake.php
@@ -3,11 +3,17 @@ declare(strict_types = 1);
 namespace Klapuch\Ini;
 
 final class Fake implements Ini {
+    private $ini;
+
+    public function __construct(array $ini = []) {
+        $this->ini = $ini;
+    }
+
     public function read(): array {
-        
+        return $this->ini;
     }
 
     public function write(array $values) {
-
+        $this->ini = array_merge($this->ini, $values);
     }
 }
